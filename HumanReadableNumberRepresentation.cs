406e0b9cd560be2b37761e8675b6fc97babf2733
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2017-12-26 20:34:10 -0700

    C# Files

---
+++ HumanReadableNumberRepresentation.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Text;
+
+public class Test
+{
+   /* public static void Main()
+    {
+        while (true)
+        {
+            long value = long.Parse(Console.ReadLine());
+            int decimalPlaces = int.Parse(Console.ReadLine());
+            bool addDecimalForSingleDigit = (Console.ReadLine() == "1") ? true : false;
+            string output = GraduatedValue(value, decimalPlaces, addDecimalForSingleDigit);
+            Console.WriteLine(output);
+            Console.WriteLine("Press any key to quit...");
+            //Console.ReadKey();
+        }
+        
+    } */
+
+    public static string GraduatedValue(long value, int decimalPlaces, bool addDecimalForSingleDigit)
+    {
+        string tempVal = value.ToString();
+        int numLen = tempVal.Length;
+
+        Boolean singleK = false;
+
+        String repFormat = "";
+
+        long quotient = 0;
+        double valueInK = 0;
+
+
+        // specially handle number between -999 and 1000
+
+        if (value >= -999 && value < 1000)
+        {
+            if (addDecimalForSingleDigit)
+                decimalPlaces++;
+
+            if (value != 0) // for non zero number
+            {
+                double valueLessThanOneK = Math.Round((double)value, decimalPlaces, MidpointRounding.ToEven);
+                return valueLessThanOneK.ToString();
+            }
+            else // for 0
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("0");
+
+                for (int i = 0; i < decimalPlaces; i++)
+                {
+                    if (i == 0)
+                        sb.Append(".0");
+                    else
+                        sb.Append("0");
+                }
+
+                return sb.ToString();
+
+            }
+        }
+
+        // convert negative number into positive number to process in the same way as positive numbers
+        Boolean isNegativeNumber = false;
+
+        if (value < -999)
+        {
+            isNegativeNumber = true;
+            value = value * -1;
+        }
+
+
+        if (value < 1000000)
+        {
+            repFormat = "K";
+
+            quotient = value / 1000;
+            valueInK = value / 1000.0;
+
+            //Console.WriteLine(valueInK);
+
+            if (quotient <= 9)
+            {
+                singleK = true;
+
+            }
+        }
+        else if (value >= 1000000 && value < 1000000000)
+        {
+            repFormat = "M";
+            quotient = value / 1000000;
+            valueInK = value / 1000000.0;
+        }
+        else if (value >= 1000000000 && value < 1000000000000)
+        {
+            repFormat = "B";
+            quotient = value / 1000000000;
+            valueInK = value / 1000000000.0;
+        }
+        else if (value >= 1000000000000 && value < 1000000000000000)
+        {
+            repFormat = "T";
+            quotient = value / 1000000000000;
+            valueInK = value / 1000000000000.0;
+        }
+        else if (value >= 1000000000000000)
+        {
+            repFormat = "Q";
+            quotient = value / 1000000000000000;
+            valueInK = value / 1000000000000000.0;
+        }
+
+
+
+
+
+
+        double value1 = -1;
+
+        if (addDecimalForSingleDigit && singleK)
+            value1 = Math.Round(valueInK, decimalPlaces + 1, MidpointRounding.ToEven);
+
+        else if (addDecimalForSingleDigit && !singleK && decimalPlaces > 0)
+            value1 = Math.Round(valueInK, decimalPlaces + 1, MidpointRounding.ToEven);
+        else
+            value1 = Math.Round(valueInK, decimalPlaces, MidpointRounding.ToEven);
+
+
+        if (singleK)
+        {
+
+            String addExtra = "";
+
+            if (decimalPlaces == 2 && tempVal.Substring(tempVal.Length - 3).Equals("000"))
+            {
+                if (addDecimalForSingleDigit)
+                    addExtra = ".000";
+                else
+                    addExtra = ".00";
+            }
+            else if (decimalPlaces == 2 && tempVal.Substring(tempVal.Length - 2).Equals("00"))
+            {
+                if (addDecimalForSingleDigit)
+                    addExtra = "00";
+                else
+                    addExtra = "0";
+            }
+            else if (decimalPlaces == 2 && tempVal.Substring(tempVal.Length - 1).Equals("0"))
+            {
+                if (addDecimalForSingleDigit)
+                    addExtra = "0";
+            }
+
+            if (decimalPlaces == 1 && tempVal.Substring(tempVal.Length - 3).Equals("000"))
+            {
+                if (addDecimalForSingleDigit)
+                    addExtra = ".00";
+                else
+                    addExtra = ".0";
+            }
+
+            else if (decimalPlaces == 1 && tempVal.Substring(tempVal.Length - 2).Equals("00"))
+            {
+                if (addDecimalForSingleDigit)
+                    addExtra = "0";
+            }
+            
+
+            if (isNegativeNumber)
+                return "-" + value1.ToString() + addExtra + "K";
+            else
+                return value1.ToString() + addExtra + "K";
+
+        }
+
+        if (isNegativeNumber)
+            return "-" + value1.ToString() + repFormat;
+        else
+            return value1.ToString() + repFormat;
+    }
+}
+/* TestGraduatedValue("Simplest Low Value", 123, 0, false, "123");
+TestGraduatedValue("Zero", 0, 0, false, "0");
+TestGraduatedValue("Decimal Zeros", 0, 4, false, "0.0000");
+
+TestGraduatedValue("Simple One Grad", 12345, 0, false, "12K");
+TestGraduatedValue("Simple With Decimals", 12345, 3, false, "12.345K");
+
+TestGraduatedValue("AddDec Shouldnt Affect", 12345, 0, true, "12K");
+TestGraduatedValue("Zero AddDec", 0, 0, true, "0.0");
+
+TestGraduatedValue("Round Integer Up", 1880, 0, false, "2K");
+TestGraduatedValue("Round Decimal Up", 1880, 1, false, "1.9K");
+TestGraduatedValue("Round After AddDec", 1880, 0, true, "1.9K");
+
+TestGraduatedValue("Bankers Rounding Up", 1500, 0, false, "2K");
+TestGraduatedValue("Bankers Rounding Down", 4500, 0, false, "4K");
+
+TestGraduatedValue("Small Negatives", -123, 0, false, "-123");
+TestGraduatedValue("Negatives", -1000, 0, false, "-1K");
+TestGraduatedValue("Negatives Bankers Up", -1500, 0, false, "-2K");
+TestGraduatedValue("Bankers Bankers Down", -4500, 0, false, "-4K");
+
+TestGraduatedValue("Large With Decimals", 9372036854775807, 1, true, "9.37Q");
+TestGraduatedValue("Large No More Grad", 9223372036854775807, 0, true, "9223Q");
+
+TestGraduatedValue("No Premature Grad", 999, 0, false, "999");*/
+
+/*Failed for Simplest Low Value: Failed with [0K] rather than [123]
+Failed for Zero: Failed with [0K] rather than [0]
+Failed for Decimal Zeros: Failed with [0K] rather than [0.0000]
+Succeeded for Simple One Grad
+Succeeded for Simple With Decimals
+Succeeded for AddDec Shouldnt Affect
+Failed for Zero AddDec: Failed with [0K] rather than [0.0]
+Succeeded for Round Integer Up
+Succeeded for Round Decimal Up
+Succeeded for Round After AddDec
+Succeeded for Bankers Rounding Up
+Succeeded for Bankers Rounding Down
+Failed for Small Negatives: Failed with [0K] rather than [-123]
+Succeeded for Negatives
+Succeeded for Negatives Bankers Up
+Succeeded for Bankers Bankers Down
+Failed for Large With Decimals: Failed with [9Q] rather than [9.37Q]
+Succeeded for Large No More Grad
+Failed for No Premature Grad: Failed with [1K] rather than [999]
+*/
---
+++ LeastUniqueFilePathUsingTree.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CustomNode
+    {
+        public string Title;
+        public CustomNode Parent;
+        public List<CustomNode> Children;
+
+
+        public CustomNode(string title, CustomNode parent)
+        {
+            Title = title;
+            Parent = parent;
+            Children = new List<CustomNode>();
+
+            if (Parent != null)
+                Parent.Children.Add(this);
+        }
+
+        public CustomNode Find(string path)
+        {
+            if (String.IsNullOrEmpty(path))
+                return null;
+
+            if (path == Title)
+                return this;
+
+            string[] pieces = path.Split(new char[] { '/' });
+
+            foreach (var child in Children)
+            {
+                if (child.Title == pieces[1])
+                    return child.Find(path.Remove(0, Title.Length + 1));
+            }
+
+            return null;
+        }
+
+        public List<String> FindAll(CustomNode nodeToBeSearched, string absolutePathSoFar)
+        {
+            if (LevelOfTheNode(this) == 0)
+                absolutePathSoFar = this.Title;
+            else
+                absolutePathSoFar = absolutePathSoFar + "/" + this.Title;
+
+            List<String> matchedNodes = new List<String>();
+
+            if (nodeToBeSearched.Title == Title)
+            {
+                matchedNodes.Add(absolutePathSoFar);
+            }
+
+            foreach (var child in Children)
+            {
+                  List<String> temp = new List<String>();
+                  temp = child.FindAll(nodeToBeSearched, absolutePathSoFar);
+
+                    if (temp.Count > 0)
+                    {
+                        matchedNodes.AddRange(temp);
+
+                    }
+                
+            }
+
+            return matchedNodes;
+        } 
+
+        public int LevelOfTheNode(CustomNode target)
+        {
+            CustomNode parentNode = target.Parent;
+            int level = 0;
+            while (parentNode != null)
+            {
+                parentNode = parentNode.Parent;
+                level++;
+            }
+            return level;
+        }
+    }
+
+    public class Test
+    {
+        public static void Main()
+        {
+            //Build a test tree (matches the example)
+            CustomNode root = new CustomNode("Root", null);
+            CustomNode userData = new CustomNode("UserData", root);
+            CustomNode ud_browser = new CustomNode("Browser", userData);
+            CustomNode ud_word = new CustomNode("Word", userData);
+            CustomNode priv = new CustomNode("Private", userData);
+            CustomNode priv_word = new CustomNode("Word", priv);
+
+            CustomNode windows = new CustomNode("Windows", root);
+            CustomNode programs = new CustomNode("Programs", root);
+            CustomNode notepad = new CustomNode("Notepad", programs);
+            CustomNode prog_word = new CustomNode("Word", programs);
+            CustomNode prog_browser = new CustomNode("Browser", programs);
+
+            CustomNode custom1 = new CustomNode(Console.ReadLine(), root);
+            CustomNode custom2 = new CustomNode(Console.ReadLine(), custom1);
+            CustomNode custom3 = new CustomNode(Console.ReadLine(), custom2);
+            CustomNode target = root.Find(Console.ReadLine());
+
+            Console.WriteLine(GetShortestUniqueQualifier(root, target));
+
+            //The following lines written to avoid compiler warnings in the given IDE. 
+            // These are actuallly not required for my own IDE
+            //getting waning like this from the default IDE: warning CS0219: The variable `ud_browser' is assigned but its value is never used
+            string temp1 = ud_browser.Title + ud_word.Title + priv_word.Title + priv_word.Title;
+            string temp2 = windows.Title + notepad.Title + prog_word.Title + prog_browser.Title;
+            string temp3 = custom3.Title;
+            temp3 = temp1 + temp2 + temp3;
+
+            Console.ReadKey();
+        }
+
+        public static string GetShortestUniqueQualifier(CustomNode root, CustomNode target)
+        {
+            if (root == null)
+                return "null";
+
+            if (target == null)
+                return "null";
+
+            if (String.IsNullOrEmpty(target.Title))
+                return "null";
+
+            if (String.IsNullOrEmpty(root.Title))
+                return "null";
+
+        
+            //reconstruct the path of the target node again. I do not want to touch the given functions
+            CustomNode parentNode = target.Parent;
+            string targetNodeAbsolutepath = target.Title;
+
+  
+            while (parentNode!=null)
+            {
+                targetNodeAbsolutepath = parentNode.Title + "/" + targetNodeAbsolutepath;
+                parentNode = parentNode.Parent;
+        
+            }
+       
+            //Search and get all conflicting paths calling this method
+            List <String> conflictingPaths = root.FindAll(target,"");
+
+            int matchedIndex = -1;
+            for (int i = 0; i < conflictingPaths.Count; i++)
+                if (conflictingPaths[i].Equals(targetNodeAbsolutepath))
+                    matchedIndex = i;
+
+            //Console.WriteLine(conflictingPaths[i]);
+
+            conflictingPaths.RemoveAt(matchedIndex);
+
+
+            int index = -1;
+            if(conflictingPaths.Count==0)
+            {
+                index = targetNodeAbsolutepath.LastIndexOf("/");
+
+                if (index >= 0)
+                    return targetNodeAbsolutepath.Substring(index + 1);
+                else
+                    return targetNodeAbsolutepath;
+            }
+
+
+            string probableIdentifier = "";
+            List<String> conflictingNames = new List<String>();
+            for (int i = 0; i < conflictingPaths.Count; i++)
+            {
+                conflictingNames.Add("");
+            }
+
+                string remainingTargetNodeAbsolutepath = targetNodeAbsolutepath;
+            string remainingConflictingNodeAbsolutepath = "";
+
+            int blankCount = 0;
+
+            while (true)
+            {
+                index = remainingTargetNodeAbsolutepath.LastIndexOf("/");
+
+                if (index >= 0)
+                {
+                    if (!String.IsNullOrEmpty(probableIdentifier))
+                        probableIdentifier = remainingTargetNodeAbsolutepath.Substring(index + 1) + "/" + probableIdentifier;
+                    else
+                        probableIdentifier = remainingTargetNodeAbsolutepath.Substring(index + 1);
+
+                    remainingTargetNodeAbsolutepath = targetNodeAbsolutepath.Substring(0,index);
+                }
+                else
+                {
+                    if (!String.IsNullOrEmpty(remainingTargetNodeAbsolutepath))
+                    {
+                        if (!String.IsNullOrEmpty(probableIdentifier))
+                            probableIdentifier =  remainingTargetNodeAbsolutepath + "/" + probableIdentifier;
+                        else
+                            probableIdentifier = remainingTargetNodeAbsolutepath;
+
+                        remainingTargetNodeAbsolutepath = "";
+                    }
+
+                }
+
+
+                if (blankCount >= conflictingPaths.Count && blankCount > 0)
+                {
+                   // if (forwardSlashCount(targetNodeAbsolutepath)== forwardSlashCount(probableIdentifier))
+                        //return "/"+ probableIdentifier;
+                   // else
+                        return probableIdentifier;
+                }
+
+
+                int index1 = -1;
+                Boolean conflictFound = false;
+
+                blankCount = 0;
+                for (int i = 0; i < conflictingPaths.Count; i++)
+                {
+                    remainingConflictingNodeAbsolutepath = conflictingPaths[i];
+
+                    index1 = remainingConflictingNodeAbsolutepath.LastIndexOf("/");
+
+                    if (index1 >= 0)
+                    {
+                        if(!String.IsNullOrEmpty(conflictingNames[i]))
+                            conflictingNames[i]= remainingConflictingNodeAbsolutepath.Substring(index1 + 1) + "/" + conflictingNames[i];
+                        else
+                            conflictingNames[i] = remainingConflictingNodeAbsolutepath.Substring(index1 + 1);
+
+                        conflictingPaths[i] = conflictingPaths[i].Substring(0, index1);
+                    }
+                    else
+                    {
+                        if(!String.IsNullOrEmpty(conflictingPaths[i]))
+                        {
+                            if (!String.IsNullOrEmpty(conflictingNames[i]))
+                                conflictingNames[i] =  remainingConflictingNodeAbsolutepath + "/" + conflictingNames[i];
+                            else
+                                conflictingNames[i] = remainingConflictingNodeAbsolutepath;
+
+                            conflictingPaths[i] = "";
+                            blankCount++;
+                        }
+
+                    }
+
+                    if (conflictingNames[i].Equals(probableIdentifier))
+                        conflictFound = true;
+                }
+
+
+                if (conflictFound == false)
+                {
+                  //  if (forwardSlashCount(targetNodeAbsolutepath) == forwardSlashCount(probableIdentifier))
+                     //   return "/" + probableIdentifier;
+                    //else
+                        return probableIdentifier;
+                }
+
+
+            }
+
+
+           // return "null";
+        }
+
+        public static int forwardSlashCount(string result)
+        {
+            int count = 0;
+
+            for (int i = 0; i < result.Length; i++)
+                if (result[i] == '/')
+                    count++;
+
+            return count++;
+        }
+    }
+
+}
